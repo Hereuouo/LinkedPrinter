1642f1bee910a4b46f0bf249f275b84682daf5ac
Heba <oorrrrhhaa34@gmail.com>
2024-02-08 20:54:44 +0300

    first commit

---
+++ LinkedPrinter.java
@@ -0,0 +1,46 @@
+package LinkedPrinter;
+
+public class LinkedPrinter 
+{
+    // Linked list stack type string
+    private LinkedStack<String> Paper;
+    
+    //1)Initialize the printer with an empty paper stack
+    
+    //inside of construcet
+    public LinkedPrinter()
+    {
+        // empty linked list called paper
+        Paper = new LinkedStack<>();
+    
+    }
+    //2)The addPaper method add pages to the paper stack 
+    public void  addPaper(String NewPaper)
+    {
+        Paper.push(NewPaper);
+        System.out.println("---Paper succesfully Added.---");
+    }
+    //3)The printDocument method Print whole document
+    public void printDocument()
+    {
+        while (!Paper.isEmpty()) // travse 
+        {
+            String TempPaper=Paper.pop();// to not mess with the original 
+            System.out.println("-=-Document printed:"+TempPaper+"---");
+        }
+        
+    }
+    //demonstrate the ability to perform the above tasks
+    // main
+    public static void main(String[]args)
+    {
+        // Create list/stack of LinkedPrinter
+        LinkedPrinter Printer=new LinkedPrinter();
+        // Add contect to Printer using addPaper
+        Printer.addPaper("page 1");
+        Printer.addPaper("page 2");
+        Printer.addPaper("page 3");
+        // Display the document using printDocument
+        Printer.printDocument();
+    }
+}
